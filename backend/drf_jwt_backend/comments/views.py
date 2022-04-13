706781d9b5ef1ef5a6d81fe3ef887d7d705f4c9d
Garrett <ga.landin89@gmail.com>
2022-04-13 16:58:18 -0500

    Created Get All, Get, Put, Post view functions for comments

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -7,26 +7,33 @@ from .models import Comment
 
 
 
-
-
-# Create your views here.
-
-
 @api_view(['GET'])
 @permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
 
 
-
-
-@api_view(['GET', 'POST'])
-def comments_list(request):
-    if request.method == 'GET':
-        comments = Comment.objects.all()
+@api_view(['GET', 'POST', 'PUT'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer= CommentSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comments = Comment.objects.filter(user_id=request.user.id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer= CommentSerializer(data= request.data)
+    elif request.method == 'PUT':    
+        serializer = CommentSerializer(Comment, data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.save()
+        return Response(serializer.data)
 
+#Maybe duplicate GET and POST for Replies and change words accordingly
