5f2260bac0d977c05a5f9c5de912815e43c6be84
psions <psionreality@gmail.com>
2022-04-13 16:08:24 -0500

    updated views file with AllowAny +get+post api views

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import status
+from .serializers import CommentSerializer, ReplySerializer
+from .models import Comment
+
+
+
+
 
 # Create your views here.
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+
+
+
+
+@api_view(['GET', 'POST'])
+def comments_list(request):
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer= CommentSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
