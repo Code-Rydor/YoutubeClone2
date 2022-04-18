366ef296bd428503aa49a8643277aa6604bf0ac6
psions <psionreality@gmail.com>
2022-04-18 07:10:03 -0500

    added new components over the weekend. Home+loginform+navbar+Registrationform+searchbar + video player components all added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,87 @@
-// General Imports
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
-
-// Pages Imports
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import AddMusicPage from "./pages/AddMusicPage/AddMusicPage";
-
-// Component Imports
-import Navbar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
-
-// Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
+import LoginForm from "./Components/LoginForm/LoginForm";
+import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import Home from "./Components/Home/Home";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [videoTitle, setVideoTitle] = useState("");
+  const [user, setUser] = useState(null);
+  const [currentVideoId, setVideoId] = useState("");
+  const [search, setSearch] = useState("");
+  const [storedUserName, setStoredUserName] = useState("");
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("token");
+
+    try {
+      const decodedUser = jwt_decode(jwt);
+      setUser(decodedUser);
+    } catch {}
+    getVideo();
+  }, []);
+
+  async function getVideo(request) {
+    let response = await axios.get(
+      ///PLACE API KEY FROM GOOGLE ///
+    );
+    console.log("getVideo function response data", response.data);
+    try {
+      setVideoId(response.data.items[0].id.videoId);
+    } catch {}
+  }
+
+
+
   return (
-    <div>
-      <Navbar />
+    <div className="App">
+      <NavBar
+        search={search}
+        setSearch={setSearch}
+        getVideo={getVideo}
+      />
       <Routes>
         <Route
+          exact
           path="/"
           element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
+            <Home
+              storedUserName={storedUserName}
+              user={user}
+              setUser={setUser}
+              videoId={currentVideoId}
+              setVideoId={setVideoId}
+              
+            />
           }
         />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/addvideo" element={<PrivateRoute><AddMusicPage /></PrivateRoute>}/>
+        <Route
+          path="home"
+          element={() => {
+            if (!user) {
+              return <LoginForm />;
+            } else {
+              return <Home user={user} />;
+            }
+          }}
+        />
+        <Route
+          path="loginform/*"
+          element={
+            <LoginForm user={user} setStoredUserName={setStoredUserName} />
+          }
+        />
+        <Route
+          exact
+          path="loginform/registration"
+          element={<RegistrationForm />}
+        />
       </Routes>
-      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoPlayer = (props) => {
+    return ( 
+        <div>
+            <iframe title="main-player" id="ytplayer" type="text/html" width="700" height="360"
+            src={`https://www.youtube.com/embed/${props.videoId}?autoplay=1`}
+            frameBorder="0"></iframe>
+        </div>
+     );
+}
